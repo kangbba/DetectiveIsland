c981688d938e3a250afc0ea17f4974263bffb03d
kangbba <sayneinteractive@gmail.com>
2024-04-15 10:22:00 +0900

    first commit

---
+++ unity_project/DetectiveIsland/Assets/Dialogue.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+
+[System.Serializable]
+public class Dialogue : MonoBehaviour
+{
+    public bool test;
+    [SerializeField] private int testText;
+}
