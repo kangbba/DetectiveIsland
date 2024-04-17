0f052fd91c3fddedf403ca1df6bda9ac2d599f8c
kangbba <sayneinteractive@gmail.com>
2024-04-17 19:50:30 +0900

    Update NodeService.cs

--- unity_project/DetectiveIsland/Assets/NodeTools/NodeService.cs
+++ unity_project/DetectiveIsland/Assets/NodeTools/NodeService.cs
@@ -31,7 +31,6 @@ public static class NodeService
         {
             return itemDemandNode.itemDemand;
         }
-<<<<<<< Updated upstream
         else if (node is PositionChangeNode positionChangeNode)
         {
             return positionChangeNode.positionChange;
@@ -39,29 +38,10 @@ public static class NodeService
         else if (node is AssetChangeNode assetChangeNode)
         {
             return assetChangeNode.assetChange;
+        } 
+        else{
+            return null;
         }
-=======
-        /*
-    else if (node is ChoiceSetNode choiceSetNode)
-    {
-        return choiceSetNode.choiceSet;
-    }
-    else if (node is ItemDemandNode itemDemandNode)
-    {
-        return itemDemandNode.itemDemand;
-    }
-    else if (node is PositionChangeNode positionChangeNode)
-    {
-        return positionChangeNode.positionChange;
-    }
-    else if (node is AssetChangeNode assetChangeNode)
-    {
-        return assetChangeNode.assetChange; 
-    }
-    return null; */
-
->>>>>>> Stashed changes
-        return null;
     }
 
 }
